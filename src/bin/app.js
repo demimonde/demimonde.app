bf3a8ba29ac09bda96881b336f9dc9521f63e6fc
Anton <anton@adc.sh>
2018-12-09 20:40:56 +0300

    add app

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,12 +1,26 @@
 import core from '@idio/core'
 
 (async () => {
-  const { app, router, url } = await core({
-
+  const { app, router, url, middleware: { bodyparser } } = await core({
+    logger: { use: true },
+    bodyparser: {},
   }, { port: process.env.PORT || 5000 })
   router.get('/', async (ctx, next) => {
     ctx.body = 'hello world'
     await next()
+  })
+  router.get('/webhook', async (ctx, next) => {
+    const token = ctx.query['hub.verify_token']
+    const c = ctx.query['hub.challenge']
+    if (token != 'test') ctx.body = ctx.query
+    else ctx.body = c
+  })
+  router.post('/webhook', bodyparser, async (ctx, next) => {
+    console.log(ctx.request.body)
+    ctx.body = ctx.request.body
+  })
+  router.get('/privacy', async (ctx, next) => {
+    ctx.body = 'Privacy Policy'
   })
   app.use(router.routes())
   console.log('Started on %s', url)
