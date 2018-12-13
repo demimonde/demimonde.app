f1c5a923c1c3fa844d5afc544bb4fc6f38bc262c
Anton <anton@adc.sh>
2018-12-13 14:47:28 +0300

    dotenv

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,15 +1,3 @@
 require('alamode')()
-const { readFileSync } = require('fs')
-try {
-  const f = `${readFileSync('.env')}`
-  const ff = f.split('\n')
-  ff.forEach(env => {
-    const [e, ...rest] = env.split('=')
-    process.stdout.write(`[+] ${e} `)
-    process.env[e] = rest.join('=')
-  })
-  process.stdout.write('\n')
-} catch (err) { /* */
-  console.log(err)
-}
+require('@demimonde/dotenv')()
 require('./app')
