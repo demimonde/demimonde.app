b2bf5489eb9a7017f1ef629b760c87670d79b411
Anton <anton@adc.sh>
2019-03-19 03:16:20 +0300

    t

--- routes/get/index.jsx
+++ routes/get/index.jsx
@@ -1,4 +1,4 @@
-const App = ({ user }) => {
+const App = ({ user, token }) => {
   return (<div>
     <p>
       Demimonde allows to publish images online.
@@ -8,14 +8,14 @@ const App = ({ user }) => {
       {user && <li><a href="/photos">My Photos</a></li>}
       {user && <li><a href="/albums">Photo Albums</a></li>}
       <li><a href="/privacy">Privacy Policy</a></li>
-      {user && <li><a href="/signout">Sign Out</a></li>}
+      {user && <li><a href={`/signout?token=${token}`}>Sign Out</a></li>}
     </ul>
   </div>)
 }
 
 export default async (ctx) => {
   const user = ctx.session.user
-  const app = (<App activeMenu="index" user={user}/>)
+  const app = (<App activeMenu="index" user={user} token={ctx.session.token}/>)
   ctx.body = ctx.Layout({
     App: app,
     session: ctx.session,
@@ -23,4 +23,3 @@ export default async (ctx) => {
 }
 
 export const aliases = ['/']
-export const middleware = r => ['session', r]
